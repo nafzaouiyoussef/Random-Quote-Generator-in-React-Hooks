f98670d5eb4e8cbe02246f9eda36bf537605eff8
youssef <nafzaouiyousssef@gmail.com>
2023-05-24 13:17:59 +0100

    commit message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Quote from './components/Quote';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Quote />
+    </div>
+  );
+}
+
+export default App;
